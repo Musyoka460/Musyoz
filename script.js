992e8d9fa019267d458334c35dc292dc9f90d27a
Musyoka460 <musyokacornellius6@gmail.com>
2025-02-21 13:21:19 +0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-const navLinks = document.querySelector('.nav-links');
-const toggleBtn = document.querySelector('.toggle-btn');
 
-toggleBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
 
-// Animate progress bars on scroll
-const progressBars = document.querySelectorAll('.progress');
-
-function animateProgressBars() {
-  progressBars.forEach((progress) => {
-    const width = progress.getAttribute('data-progress');
-    progress.style.width = `${width}%`;
-  });
-}
 // Initialize EmailJS with your User ID
 emailjs.init('Qw5mt70DcCyii4Hcj'); // Replace with your User ID
 
