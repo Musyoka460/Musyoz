d1744b6ad4f9735f2d17cffcc366330f880e734e
Musyoka460 <musyokacornellius6@gmail.com>
2025-02-27 16:31:29 +0300

    update

---
+++ more.js
@@ -0,0 +1,9 @@
+// Add functionality to the "More" button
+const moreButton = document.querySelector('.more-button');
+moreButton.addEventListener('click', () => {
+  // Example: Redirect to a graphics design page
+  window.location.href = 'graphics-design.html';
+
+  // OR: Open a modal with more details
+  // openModal();
+});
